89e2cb4dc7423d1dd3d3b7c466e01c3f7985948f
PaulJohnson1 <no>
2023-07-12 16:45:12 +0000

    crafting

--- MasterServer/src/main.rs
+++ MasterServer/src/main.rs
@@ -1,6 +1,7 @@
 use std::collections::BTreeMap;
 
 use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use rand::Rng;
 use serde::{Deserialize, Serialize};
 use serde_json::Map;
 
@@ -9,8 +10,19 @@ const CLOUD_TOKEN: &str = "cloud.eyJ0eXAiOiJKV1QiLCJhbGciOiJFZERTQSJ9.CKOM_-XtMR
 const NAMESPACE_ID: &str = "04cfba67-e965-4899-bcb9-b7497cc6863b"; // prod
 const SERVER_SECRET: &str = "ad904nf3adrgnariwpanyf3qap8unri4t9b384wna3g34ytgdr4bwtvd4y";
 
-// const PETAL_COUNT: u64 = 11;
-// const RARITY_COUNT: u64 = 8;
+fn craft(count: i64, chance: f32) -> (i64, i64) {
+    let mut successes = 0;
+    let mut count = count;
+    while count >= 5 {
+        if ((rand::thread_rng().gen_range(0..2147483647) as f64 / 2147483647.0) as f32) < chance {
+            successes += 1;
+            count -= 5;
+        } else {
+            count -= rand::thread_rng().gen_range(1..=4);
+        }
+    }
+    (count, successes)
+}
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 struct DatabaseAccount {
@@ -19,13 +31,13 @@ struct DatabaseAccount {
     pub petals: serde_json::Value,
 }
 
-async fn get_user(
+async fn user_get(
     username: &String,
     password: &String,
     use_password: bool,
 ) -> Result<String, String> {
     let url = format!("https://kv.api.rivet.gg/v1/entries?namespace_id={NAMESPACE_ID}&key={DIRECTORY_SECRET}/game/players/{username}");
-    let a = reqwest::Client::new()
+    let response = reqwest::Client::new()
         .get(url)
         .header("Authorization", format!("Bearer {CLOUD_TOKEN}"))
         .send()
@@ -35,8 +47,9 @@ async fn get_user(
         .await
         .map_err(|_| "internal2".to_string())?;
 
-    let a: serde_json::Value = serde_json::from_str(&a).expect("rivet server json valid");
+    let a: serde_json::Value = serde_json::from_str(&response).expect("rivet server json valid");
     if a["value"].is_null() {
+        println!("not a rivet account error: {response}");
         return Err("not a rivet account".to_string());
     }
 
@@ -104,8 +117,8 @@ async fn user_merge_petals(
     }
 
     let mut user: DatabaseAccount =
-        serde_json::from_str(&get_user(username, &"".to_string(), false).await?)
-            .map_err(|_| "get_user returned invalid json")?;
+        serde_json::from_str(&user_get(username, &"".to_string(), false).await?)
+            .map_err(|_| "user_get returned invalid json")?;
 
     let mut game_amounts: BTreeMap<String, serde_json::Value> =
         serde_json::from_value(user.petals).map_err(|_| "Invalid petals format")?;
@@ -123,7 +136,8 @@ async fn user_merge_petals(
         }
     }
 
-    user.petals = serde_json::to_value(&game_amounts).map_err(|_| "Failed to serialize game_amounts")?;
+    user.petals =
+        serde_json::to_value(&game_amounts).map_err(|_| "Failed to serialize game_amounts")?;
 
     let url = format!("https://kv.api.rivet.gg/v1/entries?namespace_id={NAMESPACE_ID}");
     reqwest::Client::new()
@@ -147,9 +161,68 @@ async fn user_merge_petals(
     Ok(())
 }
 
-#[get("/get_user/{username}/{password}")]
-async fn get_user_req(uri: web::Path<(String, String)>) -> impl Responder {
-    match get_user(&uri.0, &uri.1, true).await {
+async fn user_craft_petals(
+    username: &String,
+    password: &String,
+    petals: &mut Vec<(u64, u64, i64)>,
+) -> Result<(), String> {
+    let len = petals.len();
+    for i in 0..len {
+        let petal = petals[i];
+        if petal.2 < 5 {
+            return Err("must have 5 of the petal type".to_string());
+        }
+    }
+
+    let mut user: DatabaseAccount =
+        serde_json::from_str(&user_get(username, password, true).await?)
+            .map_err(|_| "user_get invalid json")?;
+
+    let mut game_amounts: BTreeMap<String, serde_json::Value> =
+        serde_json::from_value(user.petals).map_err(|_| "Invalid petals format")?;
+
+    // assert valid
+    for i in 0..len {
+        let petal = petals[i];
+        let key = format!("{}:{}", petal.0, petal.1);
+        let count = petal.2;
+
+        let entry = game_amounts
+            .entry(key)
+            .or_insert_with(|| serde_json::Value::Number(0.into()));
+
+        match entry.as_i64() {
+            Some(x) => {
+                if x < count {
+                    return Err("insufficient funds".to_string());
+                }
+            }
+            None => return Err("error reading entry".to_string()),
+        };
+    }
+
+    // craft
+    let mut craft_results = Vec::with_capacity(len);
+
+    for i in 0..len {
+        let petal = petals[i];
+        let (id, rarity) = (petal.0, petal.1);
+        let count = petal.2;
+
+        let (petals_left, new_petals) = craft(count, 0.5);
+        craft_results.push((id, rarity + 1, new_petals));
+        craft_results.push((id, rarity, petals_left - count));
+    }
+
+    // merge
+    user_merge_petals(username, &craft_results, false).await?;
+
+    Ok(())
+}
+
+#[get("/user_get/{username}/{password}")]
+async fn user_get_req(uri: web::Path<(String, String)>) -> impl Responder {
+    match user_get(&uri.0, &uri.1, true).await {
         Ok(r) => HttpResponse::Ok().body(r),
         Err(r) => HttpResponse::BadRequest().body(r),
     }
@@ -200,7 +273,39 @@ async fn user_merge_petals_req(uri: web::Path<(String, String, String)>) -> impl
         };
         merged_petals.push((id, rarity, count));
     }
-    match user_merge_petals(&uri.1.to_string(), &merged_petals, true).await {
+    match user_merge_petals(&uri.1.to_string(), &merged_petals, false).await {
+        Ok(_) => HttpResponse::Ok().body("success"),
+        Err(r) => HttpResponse::BadRequest().body(r),
+    }
+}
+
+#[get("user_craft_petals/{username}/{password}/{petals_string}")]
+async fn user_craft_petals_req(uri: web::Path<(String, String, String)>) -> impl Responder {
+    let petals_string = uri.2.to_string();
+    let mut merged_petals = vec![];
+    for entry in petals_string.split(",") {
+        let parts: Vec<&str> = entry.split(":").collect();
+        if parts.len() != 3 {
+            return HttpResponse::BadRequest().body("Invalid petal entry format");
+        }
+
+        let id: u64 = match parts[0].parse() {
+            Ok(val) => val,
+            Err(_) => return HttpResponse::BadRequest().body("Invalid id format"),
+        };
+
+        let rarity: u64 = match parts[1].parse() {
+            Ok(val) => val,
+            Err(_) => return HttpResponse::BadRequest().body("Invalid rarity format"),
+        };
+
+        let count: i64 = match parts[2].parse() {
+            Ok(val) => val,
+            Err(_) => return HttpResponse::BadRequest().body("Invalid count format"),
+        };
+        merged_petals.push((id, rarity, count));
+    }
+    match user_craft_petals(&uri.0.to_string(), &uri.1.to_string(), &mut merged_petals).await {
         Ok(_) => HttpResponse::Ok().body("success"),
         Err(r) => HttpResponse::BadRequest().body(r),
     }
@@ -211,10 +316,11 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new().service(
             web::scope("/api")
-                .service(get_user_req)
+                .service(user_get_req)
                 .service(user_create_req)
                 .service(user_exists_req)
-                .service(user_merge_petals_req),
+                .service(user_merge_petals_req)
+                .service(user_craft_petals_req),
         )
     })
     .workers(10)
