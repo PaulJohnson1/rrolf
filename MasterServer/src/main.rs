fc33720f666fb5b03f898d4ebff3bb079673c903
PaulJohnson1 <no>
2023-07-12 04:11:35 +0000

    user create and user exists

--- MasterServer/src/main.rs
+++ MasterServer/src/main.rs
@@ -1,5 +1,6 @@
 use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
-use reqwest::StatusCode;
+use serde::{Deserialize, Serialize};
+use serde_json::Map;
 /*
 a = await fetch("https://kv.api.rivet.gg/v1/entries", {
     headers: {
@@ -27,21 +28,15 @@ const NAMESPACE_ID: &str = "04cfba67-e965-4899-bcb9-b7497cc6863b"; // prod
                                                                    // const PETAL_COUNT: u64 = 11;
                                                                    // const RARITY_COUNT: u64 = 8;
 
+#[derive(Serialize, Deserialize, Debug, Clone)]
 struct DatabaseAccount {
     pub password: String,
     pub username: String,
     pub petals: serde_json::Value,
 }
 
-async fn maybe_create_account(username: &String, password: &String) -> Result<String, String> {
-    Ok("aa".to_string())
-}
-
-async fn get_user_impl(username: &String, password: &String) -> Result<String, String> {
-    maybe_create_account(&username, &password).await?;
-
+async fn get_user(username: &String, password: &String) -> Result<String, String> {
     let url = format!("https://kv.api.rivet.gg/v1/entries?namespace_id={NAMESPACE_ID}&key={DIRECTORY_SECRET}/game/players/{username}");
-
     let a = reqwest::Client::new()
         .get(url)
         .header("Authorization", format!("Bearer {CLOUD_TOKEN}"))
@@ -53,32 +48,109 @@ async fn get_user_impl(username: &String, password: &String) -> Result<String, S
         .map_err(|_| "internal2".to_string())?;
 
     let a: serde_json::Value = serde_json::from_str(&a).expect("rivet server json valid");
-    let b = &a["value"];
-    if b.is_null() {
+    if a["value"].is_null() {
         if a["watch"].is_null() {
             return Err("invalid username".to_string());
         }
-        return Err(format!("{{\"error\":\"not a rivet account?\",\"rivet_response\":{}}}", a.to_string()).to_string());
+        return Err(format!(
+            "{{\"error\":\"not a rivet account?\",\"rivet_response\":{}}}",
+            a.to_string()
+        )
+        .to_string());
     }
 
-    if b["password"].as_str().expect("password doesn't exist") != password.as_str() {
+    let b: DatabaseAccount = serde_json::from_str(&a["value"].to_string()).unwrap();
+
+    if b.password != password.as_str() {
         return Err("invalid password".to_string());
     }
-    Ok(b.to_string())
+    Ok(serde_json::to_string(&b).unwrap())
+}
+
+async fn user_exists(username: &String) -> Result<bool, String> {
+    let url = format!("https://kv.api.rivet.gg/v1/entries?namespace_id={NAMESPACE_ID}&key={DIRECTORY_SECRET}/game/players/{username}");
+    let a = reqwest::Client::new()
+        .get(url)
+        .header("Authorization", format!("Bearer {CLOUD_TOKEN}"))
+        .send()
+        .await
+        .map_err(|_| "internal1".to_string())?
+        .text()
+        .await
+        .map_err(|_| "internal2".to_string())?;
+
+    let a: serde_json::Value = serde_json::from_str(&a).expect("rivet server json valid");
+    Ok(!a["value"].is_null())
+}
+
+async fn user_create(username: &String, password: &String, safe: bool) -> Result<(), String> {
+    if safe {
+        if user_exists(username).await? {
+            return Err("account already exists".to_string());
+        }
+    }
+    let url = format!("https://kv.api.rivet.gg/v1/entries?namespace_id={NAMESPACE_ID}");
+    let r = reqwest::Client::new()
+        .put(url)
+        .body({
+            let account = DatabaseAccount {
+                username: username.to_string(),
+                password: password.to_string(),
+                petals: serde_json::Value::Object(Map::new()),
+            };
+            let mut request_values = Map::new();
+            request_values.insert("key".to_string(), serde_json::value::to_value(format!("{DIRECTORY_SECRET}/game/players/{username}")).unwrap());
+            request_values.insert("namespace_id".to_string(), serde_json::value::to_value(NAMESPACE_ID.to_string()).unwrap());
+            request_values.insert("value".to_string(), serde_json::value::to_value(account).unwrap());
+            let request_json = serde_json::Value::Object(request_values);
+            serde_json::to_string(&request_json).unwrap()
+        })
+        .header("Authorization", format!("Bearer {CLOUD_TOKEN}"))
+        .send()
+        .await
+        .map_err(|_| "internal1".to_string())?
+        .text()
+        .await
+        .map_err(|_| "internal2".to_string())?;
+    Ok(())
 }
 
 #[get("/get_user/{username}/{password}")]
-async fn get_user(uri: web::Path<(String, String)>) -> impl Responder {
-    match get_user_impl(&uri.0, &uri.1).await {
+async fn get_user_req(uri: web::Path<(String, String)>) -> impl Responder {
+    match get_user(&uri.0, &uri.1).await {
         Ok(r) => HttpResponse::Ok().body(r),
+        Err(r) => HttpResponse::BadRequest().body(r),
+    }
+}
+
+#[get("/user_create/{username}/{password}")]
+async fn user_create_req(uri: web::Path<(String, String)>) -> impl Responder {
+    match user_create(&uri.0, &uri.1, true).await {
+        Ok(_) => HttpResponse::Ok().body("success"),
+        Err(r) => HttpResponse::BadRequest().body(r),
+    }
+}
+
+#[get("/user_exists/{username}")]
+async fn user_exists_req(uri: web::Path<String>) -> impl Responder {
+    match user_exists(&uri.to_string()).await {
+        Ok(r) => HttpResponse::Ok().body(if r { "true" } else { "false" }),
         Err(r) => HttpResponse::BadRequest().body(r),
     }
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(get_user))
-        .bind(("127.0.0.1", 55554))?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new().service(
+            web::scope("/api")
+                .service(get_user_req)
+                .service(user_create_req)
+                .service(user_exists_req)
+        )
+    })
+    .workers(10)
+    .bind(("127.0.0.1", 55554))?
+    .run()
+    .await
 }
