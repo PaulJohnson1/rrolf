2efb798618cb624f33c27262cdcf03db9024d208
fractalized <bismuth>
2023-07-22 21:21:12 -0400

    color fix

--- Client/Ui/Tooltip/Petal.c
+++ Client/Ui/Tooltip/Petal.c
@@ -98,8 +98,8 @@ struct rr_ui_element *rr_ui_petal_tooltip_init(uint8_t id, uint8_t rarity)
         extra[sprintf(extra, "%.1f hp/s", RR_PETAL_DATA[id].damage * RR_PETAL_RARITY_SCALE[rarity].damage * 2.5)] = 0;
         rr_ui_container_add_element(this, rr_ui_set_justify(rr_ui_h_container_init(
                 rr_ui_container_init(), 0, 0, 2,
-                rr_ui_text_init("Self poison: ", 12, 0xffab2aad),
-                rr_ui_text_init(extra, 12, 0xffab2aad)),
+                rr_ui_text_init("Self poison: ", 12, 0xffbf29c2),
+                rr_ui_text_init(extra, 12, 0xffbf29c2)),
             -1, 0)
         );
     }
