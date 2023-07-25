d1c7647c0f2efee86ca9791bd81add9b91cce29a
fractalized <bismuth>
2023-07-25 19:20:17 -0400

    make earlier waves harder

--- Server/Waves.c
+++ Server/Waves.c
@@ -19,7 +19,7 @@ uint8_t get_rarity_from_wave(uint32_t wave)
         rarity_cap = rr_rarity_id_max;
     uint8_t rarity = 0;
     for (; rarity < rarity_cap; ++rarity)
-        if (pow(1 - (1 - RR_MOB_WAVE_RARITY_COEFFICIENTS[rarity + 1]) * 0.3, pow(1.5, wave)) >
+        if (pow(1 - (1 - RR_MOB_WAVE_RARITY_COEFFICIENTS[rarity + 1]) * 0.33, pow(1.45, wave)) >
             rarity_seed)
             break;
     return rarity;
@@ -36,11 +36,9 @@ uint8_t get_id_from_wave(uint32_t wave)
 
 int should_spawn_at(uint32_t wave, uint8_t id, uint8_t rarity)
 {
-    if (id == rr_mob_id_trex && rarity < rr_rarity_id_rare)
+    if (id == rr_mob_id_trex && rarity < rr_rarity_id_unusual)
         return 0;
-    if (id == rr_mob_id_pteranodon && rarity < rr_rarity_id_epic)
-        return 0;
-    if (id == rr_mob_id_dakotaraptor && rarity < rr_rarity_id_legendary)
+    if (id == rr_mob_id_dakotaraptor && rarity < rr_rarity_id_epic)
         return 0;
     if (id == rr_mob_id_triceratops && rarity < rr_rarity_id_unusual)
         return 0;
