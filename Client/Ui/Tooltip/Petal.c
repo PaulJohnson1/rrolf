a8da79c94af6258bba0442f46bef4e8349ecfc5a
fractalized <bismuth>
2023-07-25 19:59:07 -0400

    made bone stack

--- Client/Ui/Tooltip/Petal.c
+++ Client/Ui/Tooltip/Petal.c
@@ -128,7 +128,7 @@ struct rr_ui_element *rr_ui_petal_tooltip_init(uint8_t id, uint8_t rarity)
     else if (id == rr_petal_id_bone)
     {
         char *extra = malloc((sizeof *extra) * 8);
-        extra[sprintf(extra, "%.1f", 3.5 * RR_PETAL_RARITY_SCALE[rarity].health)] = 0;
+        extra[sprintf(extra, "%.1f", 2.5 * RR_PETAL_RARITY_SCALE[rarity].health)] = 0;
         rr_ui_container_add_element(this, rr_ui_set_justify(rr_ui_h_container_init(
                 rr_ui_container_init(), 0, 0, 2,
                 rr_ui_text_init("Damage reduction: ", 12, 0xff666666),
