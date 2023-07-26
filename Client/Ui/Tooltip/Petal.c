0d41134f55be97510d1df11134f53310746c8891
fractalized <bismuth>
2023-07-26 08:45:42 -0400

    web duration now depends on rarity as well

--- Client/Ui/Tooltip/Petal.c
+++ Client/Ui/Tooltip/Petal.c
@@ -139,10 +139,18 @@ struct rr_ui_element *rr_ui_petal_tooltip_init(uint8_t id, uint8_t rarity)
     else if (id == rr_petal_id_web)
     {
         char *extra = malloc((sizeof *extra) * 8);
-        extra[sprintf(extra, "%d", 40 + rarity * 15)] = 0;
+        extra[sprintf(extra, "%d", 50 + rarity * 15)] = 0;
         rr_ui_container_add_element(this, rr_ui_set_justify(rr_ui_h_container_init(
                 rr_ui_container_init(), 0, 0, 2,
                 rr_ui_text_init("Web radius: ", 12, 0xffafafaf),
+                rr_ui_text_init(extra, 12, 0xffffffff)),
+            -1, 0)
+        );
+        extra = malloc((sizeof *extra) * 8);
+        extra[sprintf(extra, "%d", 5 + rarity)] = 0;
+        rr_ui_container_add_element(this, rr_ui_set_justify(rr_ui_h_container_init(
+                rr_ui_container_init(), 0, 0, 2,
+                rr_ui_text_init("Web duration: ", 12, 0xff2596be),
                 rr_ui_text_init(extra, 12, 0xffffffff)),
             -1, 0)
         );
