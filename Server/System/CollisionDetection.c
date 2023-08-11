5987c9ee3cddbf0f760555022374636b111264a7
fractalized <bismuth>
2023-08-11 10:45:43 -0400

    collision revamp

--- Server/System/CollisionDetection.c
+++ Server/System/CollisionDetection.c
@@ -34,7 +34,31 @@ static void system_insert_entities(EntityIdx entity, void *_captures)
         return;
     rr_spatial_hash_insert(this->grid, entity);
 }
+static uint8_t should_entities_collide(struct rr_simulation *this, EntityIdx a,
+                                       EntityIdx b)
+{
+#define exclude(component_a, component_b)                                      \
+    if (rr_simulation_has_##component_a(this, a) &&                            \
+        rr_simulation_has_##component_b(this, b))                              \
+        return 0;                                                              \
+    if (rr_simulation_has_##component_a(this, b) &&                            \
+        rr_simulation_has_##component_b(this, a))                              \
+        return 0;
 
+    exclude(drop, drop);
+    exclude(drop, mob);
+    uint8_t team1 = rr_simulation_get_relations(this, a)->team;
+    uint8_t team2 = rr_simulation_get_relations(this, b)->team;
+    if (team1 != team2)
+        return 1; // only drop doesn't care about team
+    exclude(petal, petal);
+    exclude(petal, flower);
+    exclude(petal, mob);
+    exclude(flower, mob);
+#undef exclude
+
+    return 1;
+}
 static void grid_filter_candidates(struct rr_simulation *this,
                                    EntityIdx entity1, EntityIdx entity2,
                                    void *_captures)
@@ -52,6 +76,8 @@ static void grid_filter_candidates(struct rr_simulation *this,
     if ((delta.x * delta.x + delta.y * delta.y) <
         collision_radius * collision_radius)
     {
+        if (!should_entities_collide(this, entity1, entity2))
+            return;
         physical1->colliding_with[physical1->colliding_with_size++] = entity2;
 #ifndef RIVET_BUILD
         if (physical1->colliding_with_size >= RR_MAX_COLLISION_COUNT)
