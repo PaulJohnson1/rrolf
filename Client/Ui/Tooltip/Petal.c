ddbf04a63731f10d52121054d2e15e0f85118e63
fractalized <bismuth>
2023-08-12 09:36:30 -0400

    nerf

--- Client/Ui/Tooltip/Petal.c
+++ Client/Ui/Tooltip/Petal.c
@@ -185,7 +185,7 @@ struct rr_ui_element *rr_ui_petal_tooltip_init(uint8_t id, uint8_t rarity)
     {
         char *extra = malloc((sizeof *extra) * 8);
         extra[sprintf(extra, "%.0f",
-                      40 + RR_PETAL_RARITY_SCALE[rarity].damage * 30)] = 0;
+                      40 + RR_PETAL_RARITY_SCALE[rarity].damage * 10)] = 0;
         rr_ui_container_add_element(
             this, rr_ui_set_justify(
                       rr_ui_h_container_init(
--- Shared/Component/Petal.c
+++ Shared/Component/Petal.c
@@ -52,7 +52,7 @@ void rr_component_petal_free(struct rr_component_petal *this,
     rr_component_physical_set_x(physical, petal_phys->x);
     rr_component_physical_set_y(physical, petal_phys->y);
     rr_component_physical_set_radius(
-        physical, 40 + RR_PETAL_RARITY_SCALE[this->rarity].damage * 30);
+        physical, 40 + RR_PETAL_RARITY_SCALE[this->rarity].damage * 10);
     rr_component_physical_set_angle(physical, rr_frand() * 2 * M_PI);
     physical->mass = 1;
     physical->friction = 0;
