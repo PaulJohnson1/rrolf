6e6aa6ceb0f31811ab73c8092e49943bbf02bd8d
fractalized <bismuth>
2023-08-13 20:15:52 -0400

    fix antennae not showing

--- Client/Renderer/RenderFlower.c
+++ Client/Renderer/RenderFlower.c
@@ -54,17 +54,18 @@ void rr_component_flower_render(EntityIdx entity, struct rr_game *game, struct r
     rr_renderer_quadratic_curve_to(renderer, 0, flower->lerp_mouth, 6, 10);
     rr_renderer_stroke(renderer);
     rr_renderer_set_fill(renderer, 0xffffe763);
-    if (flower->lerp_mouth >= 8)
-        return;
-    rr_renderer_context_state_init(renderer, &state);
-    rr_renderer_translate(renderer, 0, -flower->lerp_mouth - 7.8);
-    rr_renderer_begin_path(renderer);
-    rr_renderer_move_to(renderer, -12, 0);
-    rr_renderer_line_to(renderer, 12, 0);
-    rr_renderer_line_to(renderer, 0, 6);
-    rr_renderer_line_to(renderer, -12, 0);
-    rr_renderer_fill(renderer);
-    rr_renderer_context_state_free(renderer, &state);
+    if (flower->lerp_mouth <= 8)
+    {
+        rr_renderer_context_state_init(renderer, &state);
+        rr_renderer_translate(renderer, 0, -flower->lerp_mouth - 7.8);
+        rr_renderer_begin_path(renderer);
+        rr_renderer_move_to(renderer, -12, 0);
+        rr_renderer_line_to(renderer, 12, 0);
+        rr_renderer_line_to(renderer, 0, 6);
+        rr_renderer_line_to(renderer, -12, 0);
+        rr_renderer_fill(renderer);
+        rr_renderer_context_state_free(renderer, &state);
+    }
     if (flower->face_flags & 8)
     {
         rr_renderer_translate(renderer, 0, -36);
