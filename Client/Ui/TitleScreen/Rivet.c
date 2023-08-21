140f71ebd271ae4a0399168a33f9ded7147efaf3
fractalized <bismuth>
2023-08-20 21:38:48 -0400

    lol

--- Client/Ui/TitleScreen/Rivet.c
+++ Client/Ui/TitleScreen/Rivet.c
@@ -29,9 +29,10 @@ static void link_rivet_account(struct rr_ui_element *this,
                                struct rr_game *game)
 {
     if (game->input_data->mouse_buttons_up_this_tick & 1)
-        // TODO: make it display a loading status and disable the click
+    {      // TODO: make it display a loading status and disable the click
         // funcitonality
-        rr_rivet_link_account(game->rivet_account.token, 0);
+        //rr_rivet_link_account(game->rivet_account.token, 0);
+    }
     else if (!(game->input_data->mouse_buttons & 1))
         rr_ui_render_tooltip_below(this, game->link_account_tooltip, game);
 }
