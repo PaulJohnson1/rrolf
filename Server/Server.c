e68bc67437adabfa3935c2daea5448a9299ae315
fractalized <bismuth>
2023-09-16 15:14:39 -0400

    gre

--- Server/Server.c
+++ Server/Server.c
@@ -166,6 +166,8 @@ void rr_server_client_tick(struct rr_server_client *this)
                                        encoder.current - encoder.start);
         return;
     }
+    if (this->squad != 1)
+        printf("%d dfdsads\n", this->squad);
     if (this->player_info != NULL)
     {
         if (rr_simulation_has_entity(&this->server->simulation,
@@ -284,13 +286,9 @@ void rr_server_tick(struct rr_server *this)
         return;
     rr_api_websocket_tick(this);
     rr_simulation_tick(&this->simulation);
-    uint8_t client_count = 0;
     for (uint64_t i = 0; i < RR_MAX_CLIENT_COUNT; i++)
         if (rr_bitset_get(this->clients_in_use, i) && this->clients[i].verified && this->clients[i].received_first_packet)
-        {
             rr_server_client_tick(this->clients + i);
-            ++client_count;
-        }
 
     rr_simulation_for_each_entity(
         &this->simulation, &this->simulation,
