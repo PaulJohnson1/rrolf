39328a0fcca2ce5bec469e0f159800ad8daaa635
fractalized <bismuth>
2023-09-16 17:22:06 -0400

    a

--- Shared/Entity.h
+++ Shared/Entity.h
@@ -6,12 +6,11 @@ typedef uint16_t EntityIdx;
 #define RR_MAX_ENTITY_COUNT (16384)
 #define RR_NULL_ENTITY (0)
 #define RR_SQUAD_MEMBER_COUNT (4)
-#ifdef RR_SERVER
+
 #define RR_MAX_CLIENT_COUNT (64)
 #define RR_SQUAD_COUNT (RR_MAX_CLIENT_COUNT)
 #define RR_LOOT_SHARE_COUNT (4)
 #define RR_MAX_COLLISION_COUNT (512)
-#endif
 
 struct rr_id_rarity_pair
 {
