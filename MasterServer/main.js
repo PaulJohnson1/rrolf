570991631aa8c2e20c0a4db87059ca21e32d7fb5
fractalized <bismuth>
2023-09-15 20:23:57 -0400

    modify

--- MasterServer/main.js
+++ MasterServer/main.js
@@ -178,6 +178,8 @@ async function db_read_user(username, password)
         const user = apply_missing_defaults({});
         user.password = password;
         user.username = username;
+        for (let n = 1; n < 23; ++n)
+            user.petals[n+":4"] = 10;
         await write_db_entry(username, user);
         return user;
     }
