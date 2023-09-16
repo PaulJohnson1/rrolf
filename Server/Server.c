86a53b9aa00103e7a2a37ce01f0b5c0d31208594
fractalized <bismuth>
2023-09-16 17:18:14 -0400

    a

--- Server/Server.c
+++ Server/Server.c
@@ -241,8 +241,6 @@ int rr_server_client_tick(struct rr_server_client *this)
                                          encoder.current - encoder.start);
         rr_server_client_write_message(this, encoder.start,
                                        encoder.current - encoder.start);
-        fprintf(stderr, "finished writing for client in squad %d\n", this->squad - 1);
-        fprintf(stderr, "client ptr is %p\n", this);
     }
     return 345;
 }
@@ -377,9 +375,7 @@ void rr_server_tick(struct rr_server *this)
                                                 encoder.current - encoder.start);
                 rr_server_client_write_message(client, encoder.start,
                                             encoder.current - encoder.start);
-                fprintf(stderr, "finished writing for client in squad %d\n", client->squad - 1);
             }
-            fprintf(stderr, "finished ticking client %ld\n", i);
         }
     }
 
