52e36803cf7bb21852d9568899f29d878fbae7b4
fractalized <bismuth>
2023-10-07 12:57:06 -0400

    lol

--- Client/Renderer/RenderPetal.c
+++ Client/Renderer/RenderPetal.c
@@ -16,6 +16,8 @@ void rr_component_petal_render(EntityIdx entity, struct rr_game *game, struct rr
     struct rr_component_petal *petal =
         rr_simulation_get_petal(simulation, entity);
     struct rr_component_health *health = rr_simulation_get_health(simulation, entity);
+    if (game->cache.screen_shake)
+        rr_renderer_add_color_filter(renderer, RR_RARITY_COLORS[petal->rarity], 0.25);
     rr_renderer_add_color_filter(renderer, 0xffff0000, 0.5 * health->damage_animation);
     rr_renderer_set_global_alpha(renderer, 1 - physical->deletion_animation);
     rr_renderer_scale(renderer, 1 + physical->deletion_animation * 0.5);
@@ -24,7 +26,7 @@ void rr_component_petal_render(EntityIdx entity, struct rr_game *game, struct rr
     if (rr_simulation_get_relations(simulation, entity)->team == rr_simulation_team_id_mobs)
         rr_renderer_scale(renderer, RR_MOB_RARITY_SCALING[petal->rarity].radius);
 
-    uint8_t use_cache = ((health->damage_animation < 0.1) | game->cache.low_performance_mode) & 1;
+    uint8_t use_cache = (((health->damage_animation < 0.1) | game->cache.low_performance_mode) & 1) & (1 - game->cache.screen_shake);
     if (petal->id != rr_petal_id_peas || petal->detached == 1)
         rr_renderer_draw_petal(renderer, petal->id, use_cache);
     else
