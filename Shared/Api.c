0425a69b134f0de45a75c2c82787dfa44c991d09
fractalized <bismuth>
2023-10-11 18:18:33 -0400

    nerf kraf

--- Shared/Api.c
+++ Shared/Api.c
@@ -54,33 +54,35 @@ void rr_api_get_server_alias(char const *param_1, void *game)
         {
             fetch(UTF8ToString($1) + 'user_get_server_alias/' + UTF8ToString($0)).then(function(x) {return x.text()})
             .then(function(data) {
+                if (data === '')
+                    return;
                 fetch("https://matchmaker.api.rivet.gg/v1/lobbies/join", {
-                "headers": {
-                    "Authorization":
-                        // clang-format off
-                        "Bearer pub_prod.eyJ0eXAiOiJKV1QiLCJhbGciOiJFZERTQSJ9.COPzyfqCMhDjm4W9jTEaEgoQjQm4bpQTSoibNAqQ6PIoSiIWGhQKEgoQBM-6Z-llSJm8ubdJfMaGOw.QAFVReaGxf6gfYm5NLa1FI6tLCVa2lBKCgbpmdXcuL3_okSrtYqlB9TeTTqZlYLxOMNcMyxnulzY0d5K4JTwCw"
-                    // clang-format on
-                },
-                "method": "POST",
-                "body": '{"lobby_id":"' + data + '"}'
-            })
-                .then(function(r) { return r.json(); })
-                .then(function(json) {
-                    const host = json["ports"]["default"]["hostname"];
-                    const token = "Bearer " + json["player"]["token"];
-                    const $host = _malloc(host.length + 1);
-                    const $token = _malloc(1 + token.length);
-                    HEAPU8.set(new TextEncoder().encode(host), $host);
-                    HEAPU8.set(new TextEncoder().encode(token), $token);
-                    HEAPU8[$host + host.length] = 0;
-                    HEAPU8[$token + token.length] = 0;
-                    _rr_rivet_lobby_on_find($host, $token, json["ports"]["default"]["port"], $2);
-                    _free($host);
-                    _free($token);
+                    "headers": {
+                        "Authorization":
+                            // clang-format off
+                            "Bearer pub_prod.eyJ0eXAiOiJKV1QiLCJhbGciOiJFZERTQSJ9.COPzyfqCMhDjm4W9jTEaEgoQjQm4bpQTSoibNAqQ6PIoSiIWGhQKEgoQBM-6Z-llSJm8ubdJfMaGOw.QAFVReaGxf6gfYm5NLa1FI6tLCVa2lBKCgbpmdXcuL3_okSrtYqlB9TeTTqZlYLxOMNcMyxnulzY0d5K4JTwCw"
+                        // clang-format on
+                    },
+                    "method": "POST",
+                    "body": '{"lobby_id":"' + data + '"}'
                 })
-                .catch(function(error) {
-                    _rr_rivet_lobby_on_find(0, 0, 0, $2);
-                });
+                    .then(function(r) { return r.json(); })
+                    .then(function(json) {
+                        const host = json["ports"]["default"]["hostname"];
+                        const token = "Bearer " + json["player"]["token"];
+                        const $host = _malloc(host.length + 1);
+                        const $token = _malloc(1 + token.length);
+                        HEAPU8.set(new TextEncoder().encode(host), $host);
+                        HEAPU8.set(new TextEncoder().encode(token), $token);
+                        HEAPU8[$host + host.length] = 0;
+                        HEAPU8[$token + token.length] = 0;
+                        _rr_rivet_lobby_on_find($host, $token, json["ports"]["default"]["port"], $2);
+                        _free($host);
+                        _free($token);
+                    })
+                    .catch(function(error) {
+                        _rr_rivet_lobby_on_find(0, 0, 0, $2);
+                    });
             });
         },
         param_1, RR_BASE_API_URL, game);
