8c49b5ef498f33f828a4c1bb03d51c448d558b8e
PaulJohnson1 <no>
2023-10-11 12:46:55 -0400

    dev speed

--- Server/Server.c
+++ Server/Server.c
@@ -386,7 +386,7 @@ static int handle_lws_event(struct rr_server *this, struct lws *ws, enum lws_cal
                 if ((x != 0 || y != 0) && fabsf(x) < 10000 && fabsf(y) < 10000)
                 {
                     float mag_1 = sqrtf(x * x + y * y);
-                    float scale = RR_PLAYER_SPEED * rr_fclamp((mag_1 - 25) / 50, 0, 1);
+                    float scale = (client->dev ? 10 : RR_PLAYER_SPEED) * rr_fclamp((mag_1 - 25) / 50, 0, 1);
                     x *= scale / mag_1;
                     y *= scale / mag_1;
                 }
