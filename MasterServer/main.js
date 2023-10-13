3267055e4d0acf22d50df1f13bb68cb99f9746e7
fractalized <bismuth>
2023-10-13 10:23:47 -0400

    pish

--- MasterServer/main.js
+++ MasterServer/main.js
@@ -196,7 +196,7 @@ app.get(`${namespace}/account_link/:old_username/:old_password/:username/:passwo
             return "failed";
         }
         const new_account = await db_read_user(username, password);
-        if (!new_account || !(new_account.xp > 10))
+        if (!new_account || (new_account.xp * 3 < old_account.xp))
         {
             log("account_link", [old_username, username]);
             old_account.password = hash(username + PASSWORD_SALT);
