2107c4df13fb42ef8771653668986b3790893420
fractalized <bismuth>
2023-10-21 18:06:55 -0400

    g

--- Server/Simulation.c
+++ Server/Simulation.c
@@ -120,7 +120,7 @@ static void spawn_mob(struct rr_simulation *this, uint32_t grid_x, uint32_t grid
     struct rr_component_arena *arena = rr_simulation_get_arena(this, 1);
     struct rr_maze_grid *grid = rr_component_arena_get_grid(arena, grid_x, grid_y);
     uint8_t id;
-    if (grid->spawn_function && rr_frand() < 0.5)
+    if (grid->spawn_function && rr_frand() < 1.0/3)
         id = grid->spawn_function();
     else
         id = get_spawn_id(RR_GLOBAL_BIOME, grid);
@@ -206,7 +206,7 @@ static void tick_wave(struct rr_simulation *this)
                 continue;
             //if (grid->spawn_function != NULL)
                 //time *= 4;
-            uint32_t time = 300 - (20 * (grid->player_count > 6 ? 6 : grid->player_count));
+            uint32_t time = 320 - (20 * (grid->player_count > 6 ? 6 : grid->player_count));
             if (rand() % time == 0)
                 spawn_mob(this, grid_x, grid_y);
         }
