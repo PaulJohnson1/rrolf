ca882d585756c7b060b717ff6f920418d419d6ad
fractallized <thisvideoisalie12@gmail.com>
2023-12-07 20:05:27 -0500

    Delete Server directory

--- Server/Simulation.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <Shared/SimulationCommon.h>
-
-void rr_simulation_tick(struct rr_simulation *);
-
-int rr_simulation_entity_alive(struct rr_simulation *, EntityHash); //stricter version
-
-EntityHash rr_simulation_get_entity_hash(struct rr_simulation *, EntityIdx);
--- Server/UpdateProtocol.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-struct rr_simulation;
-struct proto_bug;
-struct rr_component_player_info;
-
-void rr_simulation_write_binary(struct rr_simulation *, struct proto_bug *,
-                                struct rr_component_player_info *);
