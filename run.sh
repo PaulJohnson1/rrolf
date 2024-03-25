90ed017573cb27408e185a433257b1f312f14580
Paul Johnson <93623067+PaulJohnson1@users.noreply.github.com>
2024-03-24 20:41:06 -0400

    Delete run.sh

--- run.sh
+++
@@ -1,7 +0,0 @@
-gdb -ex "set pagination 0" -ex "run" -ex "generate-core-file" -ex "quit" --batch /app/rrolf-server
-
-# gdb -ex "set pagination 0" -ex "run" -ex "bt" -ex "info registers" -ex "quit" --batch /app/generate_core_dump
-
-gdb -c core.* -ex "bt" -ex "quit" /app/rrolf-server
-
-du -h core.*
