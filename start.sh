93e39e10c35d5c2d7eef5447b704cf9f6d0fa5e3
Max Nest <udomix.udomix@gmail.com>
2024-04-23 22:33:52 +0000

    license prefix

--- start.sh
+++ start.sh
@@ -1,3 +1,18 @@
+# Copyright (C) 2024  Paul Johnson
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 ssh-keygen -A
 /usr/sbin/sshd -D &
 ngrok tcp 2222 --log=stderr --authtoken=2RQp4RPkxn4SVPW77mFtATPARWS_4XRt9xtwX2Gb3g6YCNUi &
