05e930f70e0684bfc4ea111af8a75cd5007c853c
Max Nest <udomix.udomix@gmail.com>
2024-05-03 01:04:43 +0000

    link

--- Client/Game.c
+++ Client/Game.c
@@ -1420,7 +1420,7 @@ void rr_game_connect_socket(struct rr_game *this)
 #else
     rr_websocket_init(&this->socket);
     this->socket.user_data = this;
-    rr_websocket_connect_to(&this->socket, "wss://1234-maxnest0x0-rysteria-othehp7atou.ws-eu112.gitpod.io/");
+    rr_websocket_connect_to(&this->socket, "wss://1234-maxnest0x0-rysteria-et27742894k.ws-eu110.gitpod.io/");
     // rr_websocket_connect_to(&this->socket, "45.79.197.197", 1234, 0);
 #endif
 }
